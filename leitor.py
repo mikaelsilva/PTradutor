3089f4fa3d65a7585ff58b8173564aefc60328d4
mvs22 <mvs2@cin.ufpe.br>
2020-02-13 01:03:03 -0300

    Atualizando 13/02/2020

--- leitor.py
+++ leitor.py
@@ -1,67 +1,72 @@
 import PyPDF2
 import pandas as pd
 
-path = "c:\\Nova pasta\\\Projetos Git\\PTradutor\\Computer Organization and Design.pdf"
-nome_arquivo="Computer"
 
-text = open(path,mode='rb')
-reader = PyPDF2.PdfFileReader(text)
+path = input("Insira o path do arquivo: ")
+tipo_arquivo = input("Insira o tipo do arquivo: ")
 
-num = reader.numPages
-lista = []
+#path = "c:\\Nova pasta\\\Projetos Git\\PTradutor\\Computer Organization and Design.pdf"
 
-for i in range(num):
-    page = reader.getPage(i)
-    word = page.extractText().upper().replace(","," ").replace("."," ").replace("\n"," ")
-    word = word.replace("(","").replace(")","").replace("“","").replace("”","").replace("ʽ","").replace("ʼ","")
-    word = word.replace("?","").replace("!","").replace("[","").replace("]","").split(' ')
+nome_arquivo = 
 
-    for p in word:
-        lista.append(p)
-        #print("--",p)
+if(tipo_arquivo == 'pdf'):
+    text = open(path,mode='rb')
+    reader = PyPDF2.PdfFileReader(text)
 
-    print(i)
+    num = reader.numPages
+    lista = []
 
-print("Fim da lista")
+    for i in range(num):
+        page = reader.getPage(i)
+        word = page.extractText().upper().replace(","," ").replace("."," ").replace("\n"," ")
+        word = word.replace("(","").replace(")","").replace("“","").replace("”","").replace("ʽ","").replace("ʼ","")
+        word = word.replace("?","").replace("!","").replace("[","").replace("]","").split(' ')
 
+        for p in word:
+            lista.append(p)
+            #print("--",p)
 
-#print("A")
-#my_list = list(dict.fromkeys(lista))
-#print(my_list)
+        print(i)
 
-dicio = {}
-
-for i in range(len(lista)):
-    #print(i,lista[i])
-    dicio[lista[i]] = lista.count(lista[i])
-
-#print(dicio)
-print("Fim do Dicionario")
-
-#print(dicio)
-#PROXIMAS ETAPAS SAO:
-    #AJUSTAR O DICIONARIO PARA QUE CONTENHA A PALAVRA E QTD
-    #CRIAR UM DICIONARIO COM AS PALAVRAS COMO CHAVE, E AS TRADUÇÕES E PRONUNCIA COMO UMA TUPLA NOS VALORES
-    #CRIAR UM TERCEIRO DICIONARIO/LISTA/TUPLA (DECIDIR A MELHOR ABORDAGEM) QUE CONTENHA | PALAVRA_INGLES | QTD | TRADUÇÕES |
+    print("Fim da lista")
 
 
-tabela = [{ "PALAVRA":None,
-                "QTD_PALAVRA":0
-              }
-             ]
+    #print("A")
+    #my_list = list(dict.fromkeys(lista))
+    #print(my_list)
+
+    dicio = {}
+
+    for i in range(len(lista)):
+        #print(i,lista[i])
+        dicio[lista[i]] = lista.count(lista[i])
+
+    #print(dicio)
+    print("Fim do Dicionario")
+
+    tabela = [{ "PALAVRA":None,
+                    "QTD_PALAVRA":0
+                }
+                ]
 
 
-df = pd.DataFrame(tabela)
-df = df[["PALAVRA","QTD_PALAVRA"]]
-df.to_csv("Computer.csv",header=True,index=False)
-
-for chave,valor in dicio.items():
-    tabela = [{ "PALAVRA":chave,
-                "QTD_PALAVRA":valor
-              }
-             ]
     df = pd.DataFrame(tabela)
     df = df[["PALAVRA","QTD_PALAVRA"]]
-    df.to_csv("Computer.csv",header=False,mode='a',index=False)
+    df.to_csv("Computer.csv",header=True,index=False)
 
-print("Fim da tabela")
+    for chave,valor in dicio.items():
+        tabela = [{ "PALAVRA":chave,
+                    "QTD_PALAVRA":valor
+                }
+                ]
+        df = pd.DataFrame(tabela)
+        df = df[["PALAVRA","QTD_PALAVRA"]]
+        df.to_csv("Computer.csv",header=False,mode='a',index=False)
+
+    print("Fim da tabela")
+
+else:
+    text = open(path,mode='r')
+    reader = text.read
+
+
---
+++ tradutor.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+aux_1 = input("CSV do arquivo: ")
+aux_2 = input("CSV do tradutor: ")
+
+arq_1 = "c:\\Nova pasta\\Projetos Git\\PTradutor\\"+aux_1
+arq_2 = "c:\\Nova pasta\\Projetos Git\\PTradutor\\"+aux_2
+
+dataframe_1 = pd.read_csv(arq_1)
+dataframe_2 = pd.read_csv(arq_2)
+
+dataframe_join = pd.merge(dataframe_1, dataframe_2, on='PALAVRA', how='inner')
+
+dataframe_join = dataframe_join.sort_values(by=['QTD_PALAVRA'],ascending=False)
+print(dataframe_join.head(10))
+
+dataframe_join.to_csv("C:\\Nova pasta\\Projetos Git\\PTradutor\\"+aux_1.split(".csv")[0]+"_Traduzido.csv")
+
+
+
+
+
+
+
+
+
+
